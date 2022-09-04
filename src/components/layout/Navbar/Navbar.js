0417fb6da62f932d8958b0914890b805bab014b8
barzilaysapir <barzilaysapir@gmail.com>
2022-09-04 10:15:46 +0300

    active navlink uselocation pathname

--- src/components/layout/Navbar/Navbar.js
+++ src/components/layout/Navbar/Navbar.js
@@ -1,18 +1,25 @@
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useLocation } from "react-router-dom";
 import { NavLink, StyledNavbar } from "./Navbar.styled";
 
 const Navbar = props => {
+    const { pathname } = useLocation();
 
     return (
         <StyledNavbar>
-            <NavLink as={RouterLink} to="/">
+            <NavLink
+                as={RouterLink}
+                to="/"
+                isActive={pathname === '/'}>
                 Home
             </NavLink>
-            <NavLink as={RouterLink} to="/about">
+            <NavLink
+                as={RouterLink}
+                to="/about"
+                isActive={pathname === '/about'}>
                 About
             </NavLink>
         </StyledNavbar>
-    )
+    );
 };
 
 export default Navbar;
--- src/components/layout/Navbar/Navbar.styled.js
+++ src/components/layout/Navbar/Navbar.styled.js
@@ -1,10 +1,11 @@
-import styled from "styled-components/macro";
+import styled, { css } from "styled-components/macro";
 import { Link } from "../../common";
 
 export const StyledNavbar = styled.nav`
     background-color: ${({ theme }) => theme.secondary};
     display: flex;
     justify-content: center;
+    align-items: center;
     gap: 20px;
 `;
 
@@ -12,4 +13,9 @@ export const NavLink = styled(Link)`
     padding: 20px 0;
     font-weight: 600;
     text-decoration: none;
+
+    ${({ isActive }) => isActive && css`
+        text-decoration: underline;
+        font-size: 17px;
+    `}
 `;
