82c0a174b241324ca82033ceab796b73ac77a15a
barzilaysapir <barzilaysapir@gmail.com>
2022-09-09 11:21:44 +0300

    install & implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import GlobalStyle from './components/GlobalStyle';
 import Home from './components/pages/Home/Home';
 import Theme from './components/Theme';
+import { Switch, Route } from 'react-router-dom';
+import About from './components/pages/About/About';
+import Navbar from './components/pages/Navbar/Navbar';
 
 function App() {
   return (
     <Theme>
       <GlobalStyle />
-      <Home />
+
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+      </Switch>
+
     </Theme>
   );
 }
